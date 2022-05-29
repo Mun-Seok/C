a4af9bbd1d4d70405b2b83e051860d6ea98f5ede
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-29 19:02:30 +0900

    day01

---
+++ day01/exam-01-02.c
@@ -0,0 +1,13 @@
+/*
+파일명:exam-01-02.c
+작성자:최문석
+수정날짜:2022-02-15
+기능:printf함수를 통한 문자열 출력
+*/
+#include <stdio.h>//링킹- 파일에 정의된 함수를 가져올수있음
+
+int main(void)
+{
+	printf("Hello!");//Hello를 출력합니다.
+	return 0;
+}
