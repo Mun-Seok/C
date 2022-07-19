91d38bed34aa04b2255e295997d240c4838ab1b6
ChoiMunSeok <105057510+Mun-Seok@users.noreply.github.com>
2022-07-19 11:39:34 +0900

    day03

---
+++ day03/exam-04-02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("\"I am a boy.\"\n");
+	printf("\'I am a boy.\'\n");
+	printf("I\tam\ta\tboy\t\n");
+	printf("I\b am a boy.\n");
+	printf("I am a boy\\\?\n");
+	return 0;
+}
